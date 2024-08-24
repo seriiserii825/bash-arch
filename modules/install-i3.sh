a9c1b03b7f6879383ecf7a92bf8981152ca8868c
serii <seriiburduja@gmail.com>
2024-08-24 12:10:20 +0300

    upd: update i3

--- modules/install-i3.sh
+++ modules/install-i3.sh
@@ -1,5 +1,21 @@
-# sudo pacman -S xorg xorg-xinit alacritty vifm rofi mousepad polybar openssh
-# sudo pacman -S xclip xsel xdotool git curl wget vi
-# sudo pacman -S ttf-dejavu awesome-terminal-fonts
-# sudo pacman -S gparted mtools dosfstools gnome-keyring 
-# sudo pacman -S lazygit
+sudo pacman -S vi vifm python 
+mkdir Downloads Documents Picture
+sudo pacman -S xorg xorg-xinit rofi i3 polybar mousepad polkit dunst lxsession sddm mesa-libgl xterm xorg-apps
+sudo systemctl enable sddm.service
+cd
+cp /etc/X11/xinitrc > ~/.xinitrc
+cat <<TEST >> ".xinitrc"
+exec i3 &
+dunst &
+lxsession
+TEST
+
+chmod +x .xinitrc
+
+cp /etc/X11/xinit/xserverrc ~/.xserverrc
+
+echo "exec /usr/bin/Xorg -nolisten tcp "$@" vt$XDG_VTNR >.xserverrc
+
+sudo systemctl start  polkit.service
+sudo systemctl enable polkit.service
+sudo systemctl status polkit.service
