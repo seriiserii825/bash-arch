87644b0a6d6bff7cc44dc684d53f18befbd9ebea
Serii <seriiburduja@gmail.com>
2024-08-25 13:29:48 +0300

    feat: bash repos wp wc plugins-wp

--- modules/bash-repos.sh
+++ modules/bash-repos.sh
@@ -1,3 +1,11 @@
 cd ~/Documents
 git clone git@github.com:seriiserii825/bash.git ~/Documents/bash
 git clone git@github.com:seriiserii825/bash-info.git ~/Documents/bash-info
+git clone git@bitbucket.org:seriiserii825/plugins-wp.git ~/Documents/plugins-wp
+mkdir ~/Documents/wordrpess
+wget -O ~/Documents/wordrpess/wordpress.zip  https://ru.wordpress.org/latest-ru_RU.zip 
+wget -O ~/Documents/wordrpess/woocommerce.zip https://downloads.wordpress.org/plugin/woocommerce.latest-stable.zip 
+unzip ~/Documents/wordrpess/wordpress.zip -d ~/Documents/wordrpess
+rm ~/Documents/wordrpess/wordpress.zip
+unzip ~/Documents/wordrpess/woocommerce.zip -d ~/Documents/wordrpess
+rm ~/Documents/wordrpess/woocommerce.zip
