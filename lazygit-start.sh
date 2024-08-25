d4f28b931daf9184f4e333c7c8ef5c9e22258391
Serii <seriiburduja@gmail.com>
2024-08-25 16:02:07 +0300

    feat: lazygit

--- lazygit-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# if in ~/.config have symlink lazygit
-if [ ! -L ~/.config/lazygit ]; then
-    ln -s ~/xubuntu/lazygit ~/.config/lazygit
-    # in ~/.config/i3/config comment line exec_always --no-startup-id ~/.config/lazygit/lazygit-start.sh
-    sed -i 's/^exec_always --no-startup-id ~/.config/lazygit/lazygit-start.sh/#exec_always --no-startup-id ~/.config/lazygit/lazygit-start.sh/' ~/.config/i3/config
-fi
