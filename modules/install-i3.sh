863e47f2cb9fdfac4da5335ebffb9775d1655f17
Serii <seriiburduja@gmail.com>
2024-08-25 13:09:06 +0300

    feat: gnome-polkit

--- modules/install-i3.sh
+++ modules/install-i3.sh
@@ -1,12 +1,9 @@
 cd 
 sudo pacman -S vi vifm python 
 mkdir Downloads Documents Picture
-sudo pacman -S xorg xorg-xinit rofi i3 polybar mousepad polkit dunst lxsession xorg-apps libnotify alacritty dmenu copyq openssh xclip xsel jq bat wget curl gnome-keyring
-
+sudo pacman -S xorg xorg-xinit rofi i3 polybar mousepad polkit dunst lxsession xorg-apps libnotify alacritty dmenu copyq openssh xclip xsel jq bat wget curl gnome-keyring polkit-gnome
 sudo pacman -S ttf-dejavu awesome-terminal-fonts
-
 sudo pacman -S bluez bluez-utils pulseaudio-bluetooth usbutils
-
 cd
 cp /etc/X11/xinitrc > ~/.xinitrc
 cat <<TEST >> ".xinitrc"
@@ -14,16 +11,10 @@ exec i3 &
 dunst &
 lxsession
 TEST
-
 chmod +x .xinitrc
-
 cp /etc/X11/xinit/xserverrc ~/.xserverrc
-
 echo "exec /usr/bin/Xorg -nolisten tcp "$@" vt$XDG_VTNR >.xserverrc"
-
 sudo systemctl start  polkit.service
 sudo systemctl enable polkit.service
 sudo systemctl status polkit.service
-
 sudo reboot now
-
