b62173c29740723e3c86366fab1d9ead63838439
serii burduja <seriiburduja@gmail.com>
2024-10-09 20:27:02 +0300

    feat: wlogout

--- modules/hyprland.sh
+++ modules/hyprland.sh
@@ -1,2 +1,7 @@
 # sudo pacman -Syu
-sudo pacman -S hyprland wofi waybar
+# sudo pacman -S hyprland wofi waybar 
+# yay -S wlogout
+
+mkdir -p ~/.config/wlogout
+sudo cp /etc/wlogout/layout ~/.config/wlogout/
+sudo cp /etc/wlogout/style.css ~/.config/wlogout/
