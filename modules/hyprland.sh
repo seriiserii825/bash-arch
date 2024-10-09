4d7da6ca581deae9533a6f51864457fc88829b78
serii burduja <seriiburduja@gmail.com>
2024-10-09 20:38:28 +0300

    feat: hyprland

--- modules/hyprland.sh
+++ modules/hyprland.sh
@@ -1,7 +1,14 @@
 # sudo pacman -Syu
-# sudo pacman -S hyprland wofi waybar 
+# sudo pacman -S hyprland wofi waybar swaylock
 # yay -S wlogout
 
-mkdir -p ~/.config/wlogout
-sudo cp /etc/wlogout/layout ~/.config/wlogout/
-sudo cp /etc/wlogout/style.css ~/.config/wlogout/
+# cd ~/.config
+#
+# ln -s ~/xubuntu/hyprland/hypr
+# ln -s ~/xubuntu/hyprland/swaylock
+# ln -s ~/xubuntu/hyprland/waybar
+# ln -s ~/xubuntu/hyprland/wlogout
+
+# mkdir -p ~/.config/wlogout
+# sudo cp /etc/wlogout/layout ~/.config/wlogout/
+# sudo cp /etc/wlogout/style.css ~/.config/wlogout/
