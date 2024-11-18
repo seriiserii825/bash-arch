916b4d25821f57040f40e013079a67e61bf4c34f
serii burduja <seriiburduja@gmail.com>
2024-11-18 10:12:39 +0200

    feat: jpeoptim

--- modules/install-i3.sh
+++ modules/install-i3.sh
@@ -4,7 +4,7 @@ sudo touch /etc/pip.conf
 sudo echo "[global]\nbreak-system-packages = true" >> /etc/pip.conf
 
 mkdir Downloads Documents Picture
-sudo pacman -S xorg xorg-xinit rofi i3 polybar mousepad polkit dunst lxsession xorg-apps libnotify alacritty dmenu copyq openssh xclip xsel jq bat wget curl gnome-keyring polkit-gnome unzip zip nitrogen feh linux-firmware libmagick
+sudo pacman -S xorg xorg-xinit rofi i3 polybar mousepad polkit dunst lxsession xorg-apps libnotify alacritty dmenu copyq openssh xclip xsel jq bat wget curl gnome-keyring polkit-gnome unzip zip nitrogen feh linux-firmware libmagick jpegoptim
 sudo pacman -S ttf-dejavu awesome-terminal-fonts woff2 dosfstools
 cd
 cp /etc/X11/xinitrc > ~/.xinitrc
